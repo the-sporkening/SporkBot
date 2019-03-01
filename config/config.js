c01bcc98be914abecda5abbf64b39ae8732dc93e
Stephen Freerking <stephenfreerking@gmail.com>
2019-03-01 12:07:58 -0800

    Config fix to postgres

--- config/config.js
+++ config/config.js
@@ -15,10 +15,8 @@ module.exports = {
 		'dialect': 'mysql',
 	},
 	'production': {
-		'username': process.env.DB_USER,
-		'password': process.env.DB_PASS,
-		'database': process.env.DB_TABLE,
-		'host': process.env.DB_HOST,
-		'dialect': 'mysql',
+		'use_env_variable': process.env.DATABASE_URL,
+		'dialect': 'postgres',
+		'protocol': 'postgres',
 	},
 };

b9d763a437638bb4b2451dde792e67eae6573d29
Stephen Freerking <stephenfreerking@gmail.com>
2019-03-01 11:59:01 -0800

    Fixed config to use url
    added sequelize cli

--- config/config.js
+++ config/config.js
@@ -15,10 +15,6 @@ module.exports = {
 		'dialect': 'mysql',
 	},
 	'production': {
-		'username': process.env.DB_USER,
-		'password': process.env.DB_PASS,
-		'database': process.env.DB_TABLE,
-		'host': process.env.DB_HOST,
-		'dialect': 'mysql',
+		'use_env_variable': process.env.DATABASE_URL,
 	},
 };

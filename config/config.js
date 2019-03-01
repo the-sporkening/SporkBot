1d3c8e77e4b9bb4cd2a3dc22683ea8263070e767
Stephen Freerking <stephenfreerking@gmail.com>
2019-03-01 12:13:19 -0800

    Fixed db issues

--- config/config.js
+++ config/config.js
@@ -15,8 +15,10 @@ module.exports = {
 		'dialect': 'mysql',
 	},
 	'production': {
-		'use_env_variable': process.env.DATABASE_URL,
+		'username': process.env.DB_USERNAME,
+		'password': process.env.DB_PASSWORD,
+		'database': process.env.DB_DATABASE,
+		'host': process.env.DB_HOST,
 		'dialect': 'postgres',
-		'protocol': 'postgres',
 	},
 };

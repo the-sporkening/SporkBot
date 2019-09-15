518fc1d6ad5ee7476e8fa21f564c7ecdc7373d42
Stephen F <snipeydev@gmail.com>
2019-09-15 13:32:27 -0700

    fixed logger issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { ShardingManager } = require('discord.js');
 require('dotenv').config();
 const path = require('path');
-const Logger = require(path.join(__dirname + '/util/Logger'));
+const Logger = require(path.join(__dirname + '/util/logger'));
 const manager = new ShardingManager(path.join(__dirname + '/bot.js'), {
 	token: process.env.DISCORD_TOKEN,
 	shardArgs: ['--ansi', '--color', '--trace-warnings'],
